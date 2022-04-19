0c3bc7f8ceed61ea70a8455f90973465524a2bdb
A7Aima <aimaabhilash@gmail.com>
2022-04-19 13:16:39 +0530

    My First app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my_first_web_app';
+  title = 'My First Web App';
+  name = "Put the name below"
 }
